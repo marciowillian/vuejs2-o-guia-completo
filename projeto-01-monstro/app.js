9ada03cacb22e52a1861c82b42260c3e865f3f03
Marcio Willian <willian_mw.cc@hotmail.com>
2020-07-08 20:06:02 -0300

    D - index/app/style - Criando area de resultado

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,11 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife: 100,
-        monsterLife: 100,
+        playerLife: 0,
+        monsterLife: 0,
     },
     computed: {
-
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        },
+        win(){
+            return this.monsterLife <= 0 && this.playerLife > 0
+        },
+        lose(){
+            return this.monsterLife > 0 && this.playerLife <= 0
+        }
     },
     methods: {
 
