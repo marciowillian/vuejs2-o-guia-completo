043fe56b2c680fd6fb5cba244973c36b13aa55fe
Marcio Willian <willian_mw.cc@hotmail.com>
2020-07-08 21:48:32 -0300

    M - app.js - Melhorando lógica do jogo

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,8 +1,10 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife: 0,
-        monsterLife: 0,
+        running: false,
+        habilitarEspecial: true,
+        playerLife: 100,
+        monsterLife: 100,
     },
     computed: {
         hasResult(){
@@ -16,7 +18,49 @@ new Vue({
         }
     },
     methods: {
+        run() {
+            this.playerLife = 100
+            this.monsterLife = 100
+            this.running = true
+        },
+        stop() {
+            this.playerLife = 100
+            this.monsterLife = 100
+            this.running = false
+        },
+        attack(especial) {
+            
+            if(especial == true){
+                this.playerLife = this.playerLife - this.getRandom(5, 10)
+                if (this.playerLife < 0) this.playerLife = 0
 
+                this.monsterLife = this.monsterLife - this.getRandom(10, 15)
+                if(this.monsterLife < 0) this.monsterLife = 0
+
+                this.isUseEspecialAttack();
+
+                console.log('Usando espécial')
+            }else {
+                this.playerLife = this.playerLife - this.getRandom(5, 10)
+                if (this.playerLife < 0) this.playerLife = 0
+
+                this.monsterLife = this.monsterLife - this.getRandom(3, 5)
+                if(this.monsterLife < 0) this.monsterLife = 0
+
+            }
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        isUseEspecialAttack() {
+            setTimeout(function() {
+                if(this.habilitarEspecial == true){
+                    this.habilitarEspecial = false
+                }
+
+            }, 10000)
+        }
     },
     watch: {
 
