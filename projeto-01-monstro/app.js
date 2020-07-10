a2b18d63c3532da79f60f3031c24d5f06b4f8f2b
Marcio Willian <willian_mw.cc@hotmail.com>
2020-07-09 22:51:30 -0300

    D - Conclusão - Programando logs do jogo

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -5,6 +5,7 @@ new Vue({
         habilitarEspecial: true,
         playerLife: 100,
         monsterLife: 100,
+        logs: [],
     },
     computed: {
         hasResult(){
@@ -25,6 +26,7 @@ new Vue({
             this.playerLife = 100
             this.monsterLife = 100
             this.running = true
+            this.logs = []
         },
         stop() {
             this.playerLife = 100
@@ -32,42 +34,32 @@ new Vue({
             this.running = false
         },
         attack(especial) {
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0)
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
             
-            if(especial == true){
-                this.playerLife = this.playerLife - this.getRandom(5, 10)
-                if (this.playerLife < 0) this.playerLife = 0
-
-                this.monsterLife = this.monsterLife - this.getRandom(10, 15)
-                if(this.monsterLife < 0) this.monsterLife = 0
-
-                this.isUseEspecialAttack();
-
-                console.log('Usando espécial')
-            }else {
-                this.playerLife = this.playerLife - this.getRandom(5, 10)
-                if (this.playerLife < 0) this.playerLife = 0
-
-                this.monsterLife = this.monsterLife - this.getRandom(3, 5)
-                if(this.monsterLife < 0) this.monsterLife = 0
-
-            }
         },
-        hurt(min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
-            this.playerLife = Math.max(this.playerLife - hurt, 0)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com  ${hurt}.`, cls)
+        },
+        healAndHurt(){
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
         },
-        isUseEspecialAttack() {
-            setTimeout(function() {
-                if(this.habilitarEspecial == true){
-                    this.habilitarEspecial = false
-                }
-
-            }, 10000)
+        registerLog(text, cls) {
+            this.logs.unshift({text, cls})
         }
     },
     watch: {
